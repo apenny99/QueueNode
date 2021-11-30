c4dd0d3e55328665f41f595ed55f15ee4814dd7d
apenny23 <apenny23@kentdenver.org>
2021-11-30 09:16:04 -0700

    Finished enqueue method and the lastNode private method

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -15,12 +15,28 @@ public class QueueNode<E> {
         length=0;
     }
 
+    private node lastNode(node n2){
+        if (n2.getNext()==null){
+            return n2;
+        }
+        return lastNode(n2.getNext());
+
+    }
+
+
     public void enqueue(E element){
         if(length == 0){
             root.setDataType(element);
+            length++;
         }
         if (length == 1) {
             node n=new node(element, null,root);
+            length++;
+        }
+        if(length>1){
+            node n = new node(element, null, lastNode(root));
+            lastNode(root).getPrev().setNext(n);
+            length++;
         }
     }
 
