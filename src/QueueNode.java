e0b0c719a7c0fee9caddb0fd5552d4746c183f6e
apenny23 <apenny23@kentdenver.org>
2021-11-30 08:57:57 -0700

    Started work on the enqueue method

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -6,11 +6,22 @@
  * Collaboration: Describe the collaboration that took place
  * Date: Date the program is handed in
  **/
-public class QueueNode {
+public class QueueNode<E> {
+    private node root;
+    private int length;
     
 
     public QueueNode(){
+        length=0;
+    }
 
+    public void enqueue(E element){
+        if(length == 0){
+            root.setDataType(element);
+        }
+        if (length == 1) {
+            node n=new node(element, null,root);
+        }
     }
 
 
