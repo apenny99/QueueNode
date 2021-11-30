1caf0a61cf01ec8e3e487f0f0e226e96da4f0f8a
apenny23 <apenny23@kentdenver.org>
2021-11-30 08:47:31 -0700

    default constructor and the base of the main file

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -1,2 +1,17 @@
+/**
+ * QueueNode
+ * a Queue of nodes
+ * Author: August Penny
+ * Collaborator(s): The names of anyone you collaborated with here
+ * Collaboration: Describe the collaboration that took place
+ * Date: Date the program is handed in
+ **/
 public class QueueNode {
+    
+
+    public QueueNode(){
+
+    }
+
+
 }
